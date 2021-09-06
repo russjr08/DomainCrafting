e595c02595907a2fd9aca938245822e684b2ef71
Russell Richardson <Russell@KronosAD.com>
2021-09-06 14:06:00 -0400

    Compile with 1.17.1 support
    Updated Gradle -> 7.1
    Updated Paper -> 1.17.1
    Updated Java Version -> 16

---
+++ src/main/com/kronosad/minecraft/domaincrafting/listeners/HostileSpawnListener.java
@@ -0,0 +1,12 @@
+package com.kronosad.minecraft.domaincrafting.listeners;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
+
+public class HostileSpawnListener implements Listener {
+
+    public void onEntitySpawn(EntitySpawnEvent event) {
+        
+    }
+
+}
